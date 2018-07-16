d00b19c9f0688066fa34b27ea023ffcfcbb3af58
cassiebensko03 <cassidymbensko@gmail.com>
2018-07-16 18:42:53 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,20 +1,36 @@
 class Dog
   
-attr_accessor :name
-
-  def initialize(name) 
-    
-  @name = name
   
-end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+# attr_accessor :name
 
-end
+#   def initialize(name) 
+    
+#   @name = name
+  
+# end
 
-dog_one = Dog.new("fido")
-dog_two = Dog.new("snoopy")
-dog_three = Dog.new("lassie")
+# end
 
-puts dog_two.name
-puts dog_one.name
-puts dog_three
+# dog_one = Dog.new("fido")
+# dog_two = Dog.new("snoopy")
+# dog_three = Dog.new("lassie")
+
+# puts dog_two.name
+# puts dog_one.name
+# puts dog_three
 
